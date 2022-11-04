426fc292ff956ba39a24a584c5e755db66b7976e
tunahanbg <tunahan.buyukgebiz@gazi.edu.tr>
2022-11-04 19:16:28 +0300

    Create ikizkenaryildiz.c
    ikizkenar yıldız, hesaplama sorusu

---
+++ ikizkenaryildiz.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int main(){
+   
+   
+    /*int k , n;
+    float sonuc;                     //*********** İLK SORU ***********
+
+    printf("Sayi giriniz: ");
+    scanf("%d", &n);
+
+    for(k=1 ; k <= n ; k++){
+
+        sonuc += (k*k + 1.23)/(k-0.25);
+        
+
+
+    }
+    printf("%f", sonuc);
+    */
+
+
+
+
+    /*int  j , n;
+
+    printf("Bir deger giriniz: ");           //*********** İKİNCİ SORU ***********
+
+    scanf("%d", &n);
+
+    for(int i = 1 ; i <= n ; i++ ){
+
+        for( int j =1; j <= i ; j++){
+
+            printf("#");
+        }
+        printf("\n");
+        */
+
+
+        
+
+   /*
+   int x, y, b , i;
+
+   printf("Sayi giriniz: ");//İstenilen satır sayısını giriniz.
+   scanf("%d", &i);
+        for (x = 0; x < 2*i; x++) {                                         //*********** ÜÇÜNCÜ SORU ***********
+            if( x % 2 != 0){
+              continue;}
+                
+
+
+            for (b = 2*i; b > x + 1; b--) {
+                printf(" ");
+            }
+            for (y = 0; y < x + 1; y++) {
+
+                printf("* ");
+            }
+            printf("\n");
+        }
+
+            */
+        
+
+
+
+
+   return 0;
+}
